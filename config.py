5918290da43056123e72706299faa6155a21190a
WalkerZ <orangejunehuunt@163.com>
2024-10-19 14:40:48 +0800

    gg

--- config.py
+++ config.py
@@ -8,10 +8,10 @@
 # model_file = "/Volumes/Elements/ai_models/Qwen25_7B_Instruct"
 
 # gpu_ecs
-model_file = "/saved_models/Qwen25_7B_Instruct"
-
+# model_file = "/saved_models/Qwen25_7B_Instruct"
+model_file = "/saved_models/Llama-3.2-11B-Vision-Instruct"
 
 train_log_file = "../../walkerz_logs/walker_agent/agent_train.log"
-api_log_file = "../../walkerz_logs/walker_agent/agent_app.log"
+api_log_file = "../../walkerz_logs/walker_agent/agent_api.log"
 
 
--- model.py
+++ model.py
@@ -9,9 +9,19 @@ import json
 import torch
 import datetime
 import re
-from config import model_file
+from config import model_file,api_log_file
 from caldav import DAVClient
 from icalendar import Calendar, Event
+import logging
+from transformers import MllamaForConditionalGeneration, AutoProcessor
+
+# 配置logging模块
+logging.basicConfig(
+    filename=api_log_file,  # 日志文件名
+    filemode='a',             # 追加模式
+    format='%(asctime)s - %(levelname)s - %(message)s',  # 日志格式
+    level=logging.INFO        # 日志级别
+)
 
 # Flask 初始化
 app = Flask(__name__)
@@ -25,43 +35,65 @@ else:
 
 # 加载模型
 tokenizer = AutoTokenizer.from_pretrained(model_file)
-model = AutoModelForCausalLM.from_pretrained(model_file, torch_dtype=torch.float16, device_map={"": device}) # .to("cuda")
+# model = AutoModelForCausalLM.from_pretrained(model_file, torch_dtype=torch.float16, device_map={"": device}) # .to("cuda")
+
+
+model = MllamaForConditionalGeneration.from_pretrained(
+    model_file,
+    torch_dtype=torch.bfloat16,
+    device_map={"": device},
+)
+# processor = AutoProcessor.from_pretrained(model_file)
+
 
 
 # 计算总花销
 def calculate_total_expenses(user_input):
-    input_content = "帮我计算金钱总额：%s，将结果以“{'total':xxx.xx}”格式返回，其中“xxx.xx”为计算结果的数字。" % user_input
-    print(f"\ninput_content: {input_content}\n")
+    input_content = "帮我计算：%s的金钱总额，将结果以“{\"total\":123.12}”格式返回，其中“123.12”为示例值，代表实际计算结果的数字。我不需要知道如何计算，只需要告诉我结果值即可。示例1：“吃饭8元的金钱总额，结果为{\"total\":8.00}”；示例2：“早餐10元，打车17元，买烟20元的金钱总额，结果为{\"total\":47.00}”；示例3：“早饭花了12元，买衣服花了120元，烟20元的金钱总额，结果为{\"total\":152.00}”；示例4:“早餐15元的金钱总额，结果为{\"total\":15.00}”。" % user_input
+    logging.info(f"\ninput_content: {input_content}\n")
+    # messages = [
+    #     {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+    #     {"role": "user", "content": input_content},
+    #     {"role": "assistant", "content": "你是Qwen的助手，你需要帮我计算金钱总额，注意数字的计算，不要算错。"}
+    # ]
     messages = [
-        {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
-        {"role": "user", "content": input_content}
+        {"role": "user", "content": [
+            {"type": "json"},
+            {"type": "text", "text": input_content}
+        ]}
     ]
+
+
     # 准备对话内容
     text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    inputs = tokenizer([text], return_tensors="pt").to(model.device)
+    logging.info(f"\ntext: {text}\n")
+    inputs = tokenizer([text], return_tensors="pt").to(device)
     # 生成模型回复
     with torch.no_grad():
         # output = model.generate(**inputs, max_new_tokens=256) # max_new_tokens=512
         generated_ids = model.generate(**inputs, max_new_tokens=384)
+    logging.info(f"\ngenerated_ids: {generated_ids}\n")
 
     generated_ids = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, generated_ids)
     ]
+    logging.info(f"\nlist of generated_ids: {generated_ids}\n")
 
     res_list = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+    logging.info(f"\nres_list: {res_list}\n")
     response = res_list[0]
-    print(f"\nresponse: {response}\n")
+    logging.info(f"\nresponse: {response}\n")
 
     pattern = r'\{(.*?):(.*?)\}'
     # pattern = r'```json\s*(\{.*?\})\s*```'
     match = re.search(pattern, response)
-    print(f"\nmatch: {match}\n")
+    logging.info(f"\nmatch: {match}\n")
     json_text = match.group(0)
-    print(f"\njson_text: {json_text} \n")  # 输出 {"total":383.00"}
+    logging.info(f"\njson_text: {json_text} \n")  # 输出 {"total":383.00"}
     json_data = json.loads(json_text)
 
     total_expenses = float(json_data["total"])
-    print(f"\ntotal_expenses: {total_expenses}\n")
+    logging.info(f"\ntotal_expenses: {total_expenses}\n")
     return total_expenses
 
 # 将花销总额记录到Apple Calendar
@@ -92,7 +124,7 @@ def add_expense_to_apple_calendar(total_expenses, date):
 def index():
     if request.method == 'POST':
         user_input = request.form['user_input']
-        print(f"\nuser_input: {user_input}\n")
+        logging.info(f"\nuser_input: {user_input}\n")
 
         total_expenses = calculate_total_expenses(user_input)
         current_date = datetime.datetime.now()
